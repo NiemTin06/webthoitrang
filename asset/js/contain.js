b8429575f9f8cac83d35675076755afb9101a1df
NiemTin <nnniemtin@gmail.com>
2026-03-10 10:34:50 +0700

    fixap20.3

--- asset/js/contain.js
+++ asset/js/contain.js
@@ -1,4 +1,5 @@
-export const PATH = "http://localhost:3000"
+// export const PATH = "http://localhost:3000"
+export const PATH = "https://webthoitrang-zeta.vercel.app/api"
 
 export const APIproduct = `${PATH}/products`;
 export const APIcategories = `${PATH}/categories`;
